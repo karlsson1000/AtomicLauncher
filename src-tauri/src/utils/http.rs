797cbf9fe0ce572f31c65e63b6429f7e4be8dfb1
karlsson <karlssn1000@gmail.com>
2026-08-22 11:23:25 +0200

    fix: send versioned user-agent

--- src-tauri/src/utils/http.rs
+++ src-tauri/src/utils/http.rs
@@ -7,7 +7,11 @@ fn build_client() -> reqwest::Client {
         .pool_max_idle_per_host(32)
         .pool_idle_timeout(Duration::from_secs(90))
         .tcp_keepalive(Duration::from_secs(60))
-        .user_agent("OctaneLauncher (https://github.com/karlsson1000/OctaneLauncher)")
+        .user_agent(concat!(
+            "OctaneLauncher/",
+            env!("CARGO_PKG_VERSION"),
+            " (https://github.com/karlsson1000/OctaneLauncher)"
+        ))
         .http2_keep_alive_interval(Duration::from_secs(30))
         .http2_keep_alive_timeout(Duration::from_secs(10))
         .build()
